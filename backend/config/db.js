c85468acb69b97061d775e3c00dbd94fd219c540
Ankit <ankitrajdihara123@gmail.com>
2026-01-13 17:03:12 +0530

    vercel_issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,27 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
+let isConnected = false;
 export const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(ENV.DB_URL);
-    console.log(`connected to MONGODB : ${conn.connection.host}`);
+    isConnected = true;
+
+    console.log(` MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("db not connected ");
-    process.exit(1);
+    console.error("MongoDB connection failed:", error.message);
+    throw error;
   }
 };
+// export const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(ENV.DB_URL);
+//     console.log(`connected to MONGODB : ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("db not connected ");
+//     process.exit(1);
+//   }
+// };
