0adf54e89846d70b45cc4e0e344f180502b6b730
Ankit <ankitrajdihara123@gmail.com>
2026-01-15 08:08:17 +0530

    admin_create_products

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -1,3 +1,45 @@
-export async function createProducts(req, res) {}
+import cloudinary from "../config/cloudinary.js";
+import { Product } from "../models/product.model.js";
+export async function createProducts(req, res) {
+  try {
+    const { name, description, price, stock, category } = req.body;
+    if (!name || !description || !price || !stock || !category) {
+      return res.status(400).json({
+        message: "All field are required",
+      });
+    }
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({
+        message: "at-least one image is required ",
+      });
+    }
+    if (req.files.length > 3) {
+      return res.status(400).json({
+        message: "Maximum 3 images are allowed",
+      });
+    }
+    const uploadPromises = req.files.map((file) => {
+      return cloudinary.uploader.upload(file.path, {
+        folder: "products",
+      });
+    });
+    const uploadResults = await Promise.all(uploadPromises);
+    const imageUrls = uploadResults.map((result) => result.secure_url);
+    const product = await Product.create({
+      name,
+      description,
+      price: parseFloat(price),
+      stock: parseFloat(stock),
+      category,
+      images: imageUrls,
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    console.error("error creating products", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+}
 export async function getAllProducts(req, res) {}
 export async function updateProducts(req, res) {}
